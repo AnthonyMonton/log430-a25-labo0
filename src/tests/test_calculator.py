c9c8d4d13d9d494fbd504f7df06bcd5bac146478
AnthonyMonton <anthony.monton.1@ens.etsmtl.ca>
2025-09-10 09:32:03 -0400

    added tests and CI for GitHub Actions

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -10,4 +10,32 @@ def test_app():
     my_calculator = Calculator()
     assert my_calculator.get_hello_message() == "Calculatrice"
 
-# TODO: ajoutez les tests
+def test_addition():
+    my_calculator = Calculator()
+    resultat = my_calculator.addition(2, 3)
+    assert resultat == 5
+    assert my_calculator.last_result == 5
+
+def test_subtraction():
+    my_calculator = Calculator()
+    resultat = my_calculator.subtraction(10, 4)
+    assert resultat == 6
+    assert my_calculator.last_result == 6
+
+def test_multiplication():
+    my_calculator = Calculator()
+    resultat = my_calculator.multiplication(7, 6)
+    assert resultat == 42
+    assert my_calculator.last_result == 42
+
+def test_division():
+    my_calculator = Calculator()
+    resultat = my_calculator.division(8, 2)
+    assert resultat == 4
+    assert my_calculator.last_result == 4
+
+def test_division_by_zero():
+    my_calculator = Calculator()
+    resultat = my_calculator.division(5, 0)
+    assert resultat == "Erreur : division par zéro"
+    assert my_calculator.last_result == "Error"
